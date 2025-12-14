cbe712e4893bd7ba05a410f5be7721688a2e1ceb
sherwan <sherwanuodcs@gmail.com>
2025-12-14 21:38:11 +0100

    added header to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import SimpleCard from "@/components/cards/simple-card/simple-card";
-
+import Header from "@/components/header/header";
 export default function Home() {
-  return ( 
+  return (
+    <>
+      <Header />
       <main className="">
-          <SimpleCard message="Hallo world."/>
-      </main> 
+        <SimpleCard message="Hallo world." />
+      </main>
+    </>
   );
 }
